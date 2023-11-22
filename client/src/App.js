4921403cc473e2ec5b6967c3f69b3dc47505c97b
https://github.com/AjayDumaraliya19 <141615043+AjayDeveloper19@users.noreply.github.com>
2023-11-22 10:44:07 +0530

    --> some changes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
   const mode = useSelector((state) => {
     state.global.mode;
   });
+
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
